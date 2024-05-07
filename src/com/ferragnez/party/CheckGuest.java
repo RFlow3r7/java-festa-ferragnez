537faba088488b1ef983ceff04d712dbd985aeda
RFlow3r7 <andrefiore19967@gmail.com>
2024-05-07 13:16:22 +0200

    Edit
    Esercizio da completare

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,47 @@
+package com.ferragnez.party;
+	
+public class CheckGuest {
+	
+	public static void main(String[]args) {
+	
+		String[] nomiPartecipanti = {
+		
+				 "Dua Lipa",
+			     "Paris Hilton",
+			     "Manuel Agnelli",
+			     "J-Ax",
+			     "Francesco Totti",
+			     "Ilary Blasi",
+			     "Bebe Vio",
+			     "Luis",
+			     "Pardis Zarei",
+			     "Martina Maccherone",
+			     "Rachel Zeilic"
+
+	};
+
+		Scanner input = new Scanner(System.in);	
+		
+		
+		System.out.println("Inserisci il tuo nome");
+		
+		String name = input.nextLine();
+		
+		boolean found = false;
+		
+		
+		for (String participantName : nomiPartecipanti) {
+			if (participantName.equalsIgnoreCase(name)) {
+				found = true;
+				break;
+			}
+		}
+		
+		if (found) {
+			System.out.println("Puoi Entrare");
+		} else {
+		    System.out.println("non puoi entrare");
+		}
+
+		}
+	}
